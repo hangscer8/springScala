a60fdc249b0ec6b36f03fa326be6052cbe0c84ed
hangscer <hangscer@hotmail.com>
2018-10-28 22:49:56 +0800

    init

---
+++ src/main/java/win/hangscer/springScala/SpringScalaApplication.java
@@ -0,0 +1,12 @@
+package win.hangscer.springScala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScalaApplication.class, args);
+	}
+}
---
+++ src/main/scala/win/hangscer/Main.scala
@@ -0,0 +1,5 @@
+package win.hangscer
+
+class Main {
+
+}
